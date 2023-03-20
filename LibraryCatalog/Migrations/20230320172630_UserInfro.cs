1a323aeed638d5d40d950bebb327a4603813b7f4
Mike <mike@mikedonovan.dev>
2023-03-20 10:28:53 -0700

    Add User Auth
    Co-authored-by: Zachary Cipolletta <ZackCipolletta@gmail.com>
    Co-authored-by: James Provance <jb24x7@gmail.com>
    Co-authored-by: Nick Guzy <nick.guzy76@gmail.com>

---
+++ LibraryCatalog/Migrations/20230320172630_UserInfro.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryCatalog.Migrations
+{
+    public partial class UserInfro : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
