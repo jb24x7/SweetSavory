1e2dc50871377fb916d6258b582700d53df7e4c1
Zachary <ZackCipolletta@gmail.com>
2023-03-21 10:49:48 -0700

    trying to fix error
    Co-authored-by: James Provance <jb24x7@gmail.com>

--- LibraryCatalog/Models/RoleModification.cs
+++ LibraryCatalog/Models/RoleModification.cs
@@ -7,8 +7,6 @@ namespace LibraryCatalog.Models
         [Required]
         public string RoleName { get; set; }
         public string RoleId { get; set; }
-        
-        #nullable enable
         public string[]? AddIds { get; set; }
         public string[]? DeleteIds { get; set; }
     }
